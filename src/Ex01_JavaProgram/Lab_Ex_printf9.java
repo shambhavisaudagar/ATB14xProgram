c9a5f3242b8dd7c057f9961332ebb8a46cb963db
Shambhavi Saudagar <shambhavisaudagar@gmail.com>
2025-09-14 17:17:57 +0530

    Table of 9 with printf

---
+++ src/Ex01_JavaProgram/Lab_Ex_printf9.java
@@ -0,0 +1,25 @@
+package Ex01_JavaProgram;
+
+public class Lab_Ex_printf9 {
+    public static void main(String[] args) {
+        int num=9;
+        System.out.printf("9*%d = %d \n",1,num*1);
+        System.out.printf("9*%d = %d \n",2,num*2);
+        System.out.printf("9*%d = %d \n",3,num*3);
+        System.out.printf("9*%d = %d \n",4,num*4);
+        System.out.printf("9*%d = %d \n",5,num*5);
+        System.out.printf("9*%d = %d \n",6,num*6);
+        System.out.printf("9*%d = %d \n",7,num*7);
+        System.out.printf("9*%d = %d \n",8,num*8);
+        System.out.printf("9*%d = %d \n",9,num*9);
+        System.out.printf("9*%d = %d \n",10,num*10);
+
+/* Multiline comment
+        System.out.println("With for loop");
+        int i = 1;
+        for(i=1; i <= 10 ;i++)
+        {
+            System.out.printf("9*%d = %d \n",i,i*num);
+       }*/
+    }// Single line comment
+}
