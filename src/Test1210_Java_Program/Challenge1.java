3acd306789047a305c25aa0e2f035daeb3ff84d3
Shambhavi Saudagar <shambhavisaudagar@gmail.com>
2025-10-12 11:22:47 +0530

    Challenge 1 JPA

---
+++ src/Test1210_Java_Program/Challenge1.java
@@ -0,0 +1,14 @@
+package Test1210_Java_Program;
+
+import java.sql.SQLOutput;
+
+public class Challenge1 {
+    public static void main() {
+     int i = 25;
+     double d = 3.14;
+     boolean b = true;
+     char c ='A';
+       // Integer: 25 Double: 3.14 Boolean: true Character: A
+        System.out.println("Integer: " +i +" Double: " + d + " Boolean:" +b+ " Character: " + c);
+    }
+}
