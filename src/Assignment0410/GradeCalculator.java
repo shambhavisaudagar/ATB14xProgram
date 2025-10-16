bac677e5d246c49e8293bd2824fa953fcdf2d914
Shambhavi Saudagar <shambhavisaudagar@gmail.com>
2025-10-16 07:14:21 +0530

    14-10 Task

---
+++ src/Assignment0410/GradeCalculator.java
@@ -0,0 +1,35 @@
+package Assignment0410;
+
+import java.util.Scanner;
+
+public class GradeCalculator {
+    static void main() {
+      /*Write a program that calculates and displays the letter grade
+      for a given numerical score (e.g., A, B, C, D, or F) based on
+      the following grading scale: A: 90-100, B: 80-89, C: 70-79, D: 60-69, F: 0-59*/
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Score");
+        int score = scanner.nextInt();
+        if (score >= 90 && score <=100)
+        {
+            System.out.println("Grade A");
+        }
+        else if(score >= 80 && score <=89)
+        {
+            System.out.println("Grade B");
+        }
+        else if(score >= 70 && score <=79)
+        {
+            System.out.println("Grade C");
+        }
+        else if (score >= 60 && score <=69)
+        {
+            System.out.println("Grade D");
+        }
+        else if (score >= 0 && score <=59)
+        {
+            System.out.println("Grade F");
+        }
+    }
+}
---
+++ src/Assignment0410/Multiples_Of_Three_And_Five.java
@@ -0,0 +1,24 @@
+package Assignment0410;
+
+public class Multiples_Of_Three_And_Five {
+    static void main() {
+        /*Write a program that prints numbers from 1 to 100.
+         However, for multiples of 3, print "Fizz" instead of the number,
+         and for multiples of 5, print "Buzz."
+         For numbers that are multiples of both 3 and 5, print "FizzBuzz."*/
+        for(int i = 1;i<=100;i++)
+        {
+            if((i%3==0)&&(i%5==0))
+            {
+                System.out.println(i+"FizzBuzz");
+            } else if (i%3==0) {
+                System.out.println(i+"Fizz");
+            } else if (i%5==0) {
+                System.out.println(i+"Buzz");
+            }else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
